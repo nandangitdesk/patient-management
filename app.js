3dc421011adcdaf0216f9616e624008b4cd6e2c3
nandangitdesk <patilnandan859@gmail.com>
2024-09-25 19:51:10 +0530

    modified app.js

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'dist')));
 
 app.set('views', path.join(__dirname, 'src', 'views'));
 
