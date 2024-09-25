350a008e863a0a53ede4d7ac0ee419ce18e1809e
nandangitdesk <patilnandan859@gmail.com>
2024-09-25 20:05:22 +0530

    update css

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+    content: [
+        './src/**/*.{html,ejs,js}', // Include your EJS files and other relevant directories
+        './public/**/*.{html,js}',   // Include your public directory if necessary
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
